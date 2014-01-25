0a2c542da60dd0bb3802c5a9bb921818f376a005
terrychenism <hahakuku@hotmail.com>
2014-01-25 01:18:32 -0500

    Update Q2.5 addList.cpp

--- Q2.5 addList.cpp
+++ Q2.5 addList.cpp
@@ -8,7 +8,7 @@ typedef struct node{
 
 
 node* init(int a[], int n){
-	node *head = NULL, *p;
+	node *head = NULL, *p= NULL;
 	for (int i = 0; i<n; ++i){
 		node *nd = new node();
 		nd->data = a[i];
@@ -22,27 +22,49 @@ node* init(int a[], int n){
 	return head;
 }
 
+void print(node *head){
+	while (head){
+		cout << head->data << " ";
+		head = head->next;
+	}
+	cout << endl;
+	getchar();
+}
 
+node *result = new node();
+int value = 0;
 node* plusList(node* L1, node* L2){
+	
+	
+	if (L1 != NULL)
+		value += L1->data;
 
+	if (L2 != NULL)
+		value += L2->data;
+
+	result->data = value % 10;
+
+	result = result->next;
+	
+	plusList(L1->next, L2->next);
+	
+
+	return result;
 
 }
 
 int main(){
 
 
-	int n = 4;
-	int a[] = {
-		1, 2, 9, 3
-	};
+	int n = 3;
+	int a[] = {7,1,6};
 	int m = 3;
-	int b[] = {
-		9, 9, 2
-	};
+	int b[] = {5,9,2};
 
 	node *p = init(a, n);
 	node *q = init(b, m);
 	plusList(p, q);
 
+	print(result);
 
 }
