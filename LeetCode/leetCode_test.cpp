47610798ac6c028af6032c3ffd3dfb923486028d
terrychenism <hahakuku@hotmail.com>
2014-04-07 21:01:26 -0400

    Update leetCode_test.cpp

--- LeetCode/leetCode_test.cpp
+++ LeetCode/leetCode_test.cpp
@@ -5,8 +5,9 @@ using namespace std;
 Solution sul;
 //-----------main function-------------
 void main(){
-	int a[] = { 3, 7, 4 ,9 ,5, 2, 6 ,1 };
-	int n = 8, m = 2;
+	//int a[] = { 3, 7, 4,9 ,5, 2, 6, 1  };
+	int a[]= {1,2,3,4,5};
+	int n = 5, m = 2,c=4;
 	/*detect cycle in the list*/
 	int value = sul.detectCycle(sul.initList(n, m, a))->val;
 	cout <<"The cycle begin with " 
@@ -14,7 +15,13 @@ void main(){
 	/*insert sort list*/
 	cout << "The head of sorted list is " 
 		<< sul.insertionSortList_(sul.initList(n, a))->val << endl;
+
+	cout << "reversed list is: " <<endl;
 	sul.reorderList(sul.initList(n, a));
+
+	//--------Reverse a linked list from position m to c-----
+	sul.reverseBetween(sul.initList(n, a),m,c);
+
 
 	system("pause");
 
