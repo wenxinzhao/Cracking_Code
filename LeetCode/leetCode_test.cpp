482432bd2634ebb7bfaeda348ab0d639e4af60bf
terrychenism <hahakuku@hotmail.com>
2014-04-10 17:11:08 -0400

    Update leetCode_test.cpp

--- LeetCode/leetCode_test.cpp
+++ LeetCode/leetCode_test.cpp
@@ -6,8 +6,8 @@ Solution sul;
 //-----------main function-------------
 void main(){
 	//int a[] = { 3, 7, 4,9 ,5, 2, 6, 1  };
-	int a[] = { 4,5};
-	int n = 2, m = 1, c = 2;
+	int a[] = { 1, 2,3,3,4,4,5};
+	int n = 7, m = 1, c = 2;
 	/*detect cycle in the list*/
 	int value = sul.detectCycle(sul.initList(n, m, a))->val;
 	cout << "The cycle begin with "
@@ -21,10 +21,21 @@ void main(){
 
 	//--------Reverse a linked list from position m to c-----
 	//sul.reverseBetween(sul.initList(n, a), m, c);
+
 	cout << "reverse Between " << m <<" and "<< c << endl;
 	sul.printList(sul.reverseBetween(sul.initList(n, a), m, c));
+
+	//----delete all duplicates -------------------------
+	cout << "delete duplicates II...." << endl;
+	sul.printList(sul.deleteDuplicates_(sul.initList(n, a)));
 	
-	
+	//---- duplicates are allowed at most twice----------
+	cout << "delete duplicates more than twice number...." << endl;
+	n = sul.removeDuplicates(a, n);
+	for (int i = 0; i < n; i++) cout << a[i] << " ";
+	cout << endl;
+
+
 	system("pause");
 
 }
