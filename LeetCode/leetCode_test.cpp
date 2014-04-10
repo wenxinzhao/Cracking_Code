6ba21c96b2dc8dc3519c727d9a570a04deadc18f
terrychenism <hahakuku@hotmail.com>
2014-04-09 23:01:26 -0400

    Update leetCode_test.cpp

--- LeetCode/leetCode_test.cpp
+++ LeetCode/leetCode_test.cpp
@@ -6,23 +6,25 @@ Solution sul;
 //-----------main function-------------
 void main(){
 	//int a[] = { 3, 7, 4,9 ,5, 2, 6, 1  };
-	int a[]= {1,2,3,4,5};
-	int n = 5, m = 2,c=4;
+	int a[] = { 4,5};
+	int n = 2, m = 1, c = 2;
 	/*detect cycle in the list*/
 	int value = sul.detectCycle(sul.initList(n, m, a))->val;
-	cout <<"The cycle begin with " 
-		 << value << endl;
+	cout << "The cycle begin with "
+		<< value << endl;
 	/*insert sort list*/
-	cout << "The head of sorted list is " 
+	cout << "The head of sorted list is "
 		<< sul.insertionSortList_(sul.initList(n, a))->val << endl;
 
-	cout << "reversed list is: " <<endl;
-	sul.reorderList(sul.initList(n, a));
+	//cout << "reversed list is: " << endl;
+	//sul.reorderList(sul.initList(n, a));
 
 	//--------Reverse a linked list from position m to c-----
-	sul.reverseBetween(sul.initList(n, a),m,c);
-
-
+	//sul.reverseBetween(sul.initList(n, a), m, c);
+	cout << "reverse Between " << m <<" and "<< c << endl;
+	sul.printList(sul.reverseBetween(sul.initList(n, a), m, c));
+	
+	
 	system("pause");
 
 }
