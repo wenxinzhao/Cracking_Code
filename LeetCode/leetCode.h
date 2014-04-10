d44ca7b99d2b4b4ad6abfb3f9ae72348d0529308
terrychenism <hahakuku@hotmail.com>
2014-04-10 17:11:23 -0400

    Update leetCode.h

--- LeetCode/leetCode.h
+++ LeetCode/leetCode.h
@@ -25,16 +25,39 @@ public:
 	ListNode *insertionSortList_(ListNode *head);
 
 	/*Given a singly linked list L: L0→L1→…→Ln-1→Ln,
-	  reorder it to: L0→Ln→L1→Ln-1→L2→Ln-2→…
-	  You must do this in-place without altering the nodes' values.
-	  For example,Given {1,2,3,4}, reorder it to {1,4,2,3}.*/
-	 
-	void reorderList(ListNode *head); 
+	reorder it to: L0→Ln→L1→Ln-1→L2→Ln-2→…
+	You must do this in-place without altering the nodes' values.
+	For example,Given {1,2,3,4}, reorder it to {1,4,2,3}.*/
+
+	void reorderList(ListNode *head);
 
 	//---------Reverse a linked list-------------
-	ListNode *Solution::reverseBetween(ListNode *head, int m, int n);
+	ListNode *reverseBetween(ListNode *head, int m, int n);
 
 	//-----print result----------------
 	void printList(ListNode *head);
+
+	//----delete all duplicates -------------------------
+	ListNode *deleteDuplicates(ListNode *head);
+	ListNode *deleteDuplicates_(ListNode *head);
+
+	//---- duplicates are allowed at most twice----------
+	int removeDuplicates(int A[], int n);
+
 };
+
+
+class bitmap {
+public: bitmap(int n); ~bitmap();
+		void set(int k);
+		bool val(int k);
+
+
+private: int len;
+		 unsigned int * map;
+		 unsigned int * reversemap;
+		 bool zero;
+};
+
+
 
