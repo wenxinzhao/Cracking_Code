0b6e87a845be8b7936e17b2c1193e949e7c3a205
terrychenism <hahakuku@hotmail.com>
2014-04-12 23:35:40 -0400

    Update ListNode.cpp

--- CC150/ListNode.cpp
+++ CC150/ListNode.cpp
@@ -133,3 +133,25 @@ void Solution::printList(ListNode *head){
 	}
 	cout << endl;
 }
+
+
+TreeNode* Solution::minimal(TreeNode *node){
+	if(node==NULL) return NULL;
+	while(node->left != NULL){
+		node = node->left;
+	}
+	return node;
+}
+
+TreeNode* Solution::successor(TreeNode *node){
+	if(node == NULL) return NULL;
+	if(node->right != NULL) Solution::minimal(node->right);
+	else{
+		TreeNode *tmp = node->parent;
+		while(tmp->key < node->key){
+			tmp = tmp->parent;
+		}
+		return tmp;
+	}
+
+}
