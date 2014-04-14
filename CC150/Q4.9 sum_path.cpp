c5f65f3a1f909deadbed93dd37b435cbc6f32b6f
terrychenism <hahakuku@hotmail.com>
2014-04-14 02:58:40 -0400

    Update Q4.9 sum_path.cpp

--- CC150/Q4.9 sum_path.cpp
+++ CC150/Q4.9 sum_path.cpp
@@ -11,19 +11,19 @@ int main(){
 	sul.initTree();
 	/*int a[] = {
 		0, 1, 2, 3, 4, 5, 6, 7, 8
-	};*/
+		};*/
 	int a[] = { 4, 3, 8, 5, 2, 1, 6 };
 	int n = 7;
-	sul.create_minimal_tree(sul.head, sul.p, a, 0, n-1);
+	sul.create_minimal_tree(sul.head, sul.p, a, 0, n - 1);
 	sul.getDepth(sul.head);
 	cout << "the height of tree is:" << sul.maxHeight() << endl;
 	/*for (int level = 0; level < sul.maxHeight(); level++){
 		sul.printNodeAtLevel(sul.head, level);
 		cout << endl;
-	}*/
+		}*/
 	sul.printNodeAtLevel(sul.head);
 	// Q4.6 LCA
-	cout << "common ancester is :"<<sul.common_ancestor(sul.head, sul.head->left->left, sul.head->right )->key << endl;
+	cout << "common ancester is :" << sul.common_ancestor(sul.head, sul.head->left->left, sul.head->right)->key << endl;
 	/*cout << endl << "postOrder"<<" ";
 	sul.postOrder(sul.head);
 	cout << endl << "inOrder" << " ";
@@ -31,7 +31,14 @@ int main(){
 	cout << endl << "preOrder" << " ";
 	sul.preOrder(sul.head);*/
 
-	sul.find_sum(sul.head, 7);
+	/*Write an algorithm to find the ‘next’ node (i.e., in-order successor)of
+	a given node in a binary search tree where each node has a link to its parent.*/
+
+	cout << "successor is :" << sul.successor(sul.head->right->left)->key << endl;
+
+	int sum = 8;
+	cout << "all paths which sum up to " << sum << endl;
+	sul.find_sum(sul.head, sum);
 	system("pause");
 
 }
