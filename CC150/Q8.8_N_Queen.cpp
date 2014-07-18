dd82b7b8bf4cfa9e0f0f0c54103d9eedb04158a7
terrychenism <hahakuku@hotmail.com>
2014-07-18 14:35:48 -0400

    Update Q8.8_N_Queen.cpp

--- CC150/Q8.8_N_Queen.cpp
+++ CC150/Q8.8_N_Queen.cpp
@@ -1,75 +1,56 @@
-#include<iostream>
-#include<string>
-#include<vector>
-#include<queue>
+#include <iostream>
+#include <cmath>
+#include <conio.h>
 using namespace std;
-vector<int>mi,mj;
-
-bool condition(int j,vector<int>mj ){
-	while(!mj.empty()){
-		if(j == mj.back()){
-			return false;
-			break;
-		}
-		mj.pop_back();
-	}
-	return true;
-
-}
-void add_element(int i,int jj, int n, int a[4][4]){
-	int j;
-	for(j = jj; j<n; j++){
-		if( condition(j,mj) && (i+j != mi.back() + mj.back()) && ((i-j)!=(mi.back()-mj.back()) ) ) {
-			a[i][j] = 1;
-			mi.push_back (i);
-			mj.push_back (j);
-			break;
-		}	
-	}
-	if(j == n){
-			int mii = mi.back();
-			mi.pop_back();
-			int mjj = mj.back();
-			mj.pop_back();
-			a[mii][mjj] =0;
-			add_element(i-1,mjj+1,n,a);
-	}
-	
-	
-	if(i == n-1)
-		return;	
-	add_element(i+1,0,n,a);
-	
+void PrintResult(int *arr, int n)
+{
+    for (int i = 1; i != n + 1; ++i)
+        cout << "(" << i << "," << arr[i] << ")" << " ";
+    cout << endl;
 }
 
-vector<vector<string> > solveNQueens(int n) {
-      vector<vector<string> > v;
-	  int a[4][4] = {NULL};
-	  
-	  mi.push_back (-10);
-	  mj.push_back (-100);
-
-	 // for(int i = 0; i<n;i++){
-		  add_element(0,0,n,a);
-	 // }
-
-	return v;
+bool Verify(int *arr, int i)
+{
+    /* 和前面的i - 1行比较，看当前放置位置是否合法？*/
+    for (int k = 1; k != i; ++k)
+        if (arr[k] == arr[i] || abs(i - k) == abs(arr[i] - arr[k]))
+            return false;
+    return true;
+}
+/* 虽然只用了一个一维数组，但是其中已经保存了足够的信息。
+因为每一行只能放一个皇后，所以一维数组的第i个位置存放的
+是在第i行的哪一列（第j列）上放置了皇后。这个递归函数
+每次处理一行，直到第n行（下标从1开始）。*/
+void NQueens(int *arr, int i, int n)
+{
+    /* 尝试着在第i行的第j列放置一个皇后。*/
+    for (int j = 1; j != n + 1; ++j)
+    {
+        arr[i] = j;
+        if (Verify(arr, i))
+        {
+            /* 这个递归程序的结束条件是第n行放置完毕，
+            所以，当递归函数从调用NQueens(arr, i + 1, n)返回时，
+            就是回到了第i行，继续搜索合适的位置。当第i + 1行的
+            所有位置都不能满足的时候，上面的调用就会返回，也就
+            是进行了所谓的回溯。这个回溯不需要显示的恢复以前的
+            调用环境，因为所需要的信息没有被破坏。*/
+            if (i == n)
+                PrintResult(arr, n);
+            else
+                NQueens(arr, i + 1, n);
+        }
+    }
 }
 
-//bool add_element(int j,int i, int a[4][4]){
-//	if( condition(j,mj) && (i+j != mi.back() + mj.back()) && (abs(i-j)) != abs(mi.back()-mj.back())){
-//		a[i][j] = 1;
-//		mi.push_back (i);
-//		mj.push_back (j);
-//		//break;
-//		return true;
-//	}
-//	return false;
-//
-//}
+int main()
+{
+    int n;
+ cout<<"n:"<<endl;
+    cin >> n;
+    int *arr = new int[n + 1];
+    NQueens(arr, 1, n);
 
-
-void main(){
-	solveNQueens(4);
-	system("pause");	
+    system("pause");
+    return 0;
 }
