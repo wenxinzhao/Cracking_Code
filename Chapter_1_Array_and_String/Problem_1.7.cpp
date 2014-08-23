d5e6c6d2b26fa733e4e6af910237440c491af567
terrychenism <hahakuku@hotmail.com>
2014-08-23 03:00:51 -0400

    Update Problem_1.7.cpp

--- Chapter_1_Array_and_String/Problem_1.7.cpp
+++ Chapter_1_Array_and_String/Problem_1.7.cpp
@@ -3,7 +3,24 @@
 #include <stdlib.h>
 using namespace std;
 
-
+void setZeroes(vector<vector<int> > &matrix) {
+	const size_t m = matrix.size();
+	const size_t n = matrix.back().size();
+    vector<bool> row(m, false); 
+	vector<bool> col(n, false); 
+	for (int i = 0; i < m;i++){
+		for (int j = 0; j < n; j++){
+			if (matrix[i][j] == 0)
+				col[i] = row[j] = true;			
+		}
+	}
+	for (int i = 0; i < m; i++){
+		for (int j = 0; j < n; j++){
+			if (col[i] == true || row[j] == true)
+				matrix[i][j] = 0;
+		}
+	}
+}
 
 void setZeros(int a[][4], int m, int n){
 	bool *row = new bool[m];
