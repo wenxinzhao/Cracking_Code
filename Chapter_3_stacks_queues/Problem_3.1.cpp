0940b3c53bb67fdc49834046e564a18374c76da8
terrychenism <hahakuku@hotmail.com>
2014-08-25 02:07:28 -0400

    Update Problem_3.1.cpp

--- Chapter_3_stacks_queues/Problem_3.1.cpp
+++ Chapter_3_stacks_queues/Problem_3.1.cpp
@@ -5,32 +5,36 @@ const int stackSize = 100;
 int array[3 * stackSize];
 int stackPointer[] = { -1, -1, -1 };
 
-int element(int stackNum){
-	return (stackNum-1)*stackSize + stackPointer[stackNum];
-}
+class stack{
+public:
+	int element(int stackNum){
+		return (stackNum - 1)*stackSize + stackPointer[stackNum];
+	}
 
-void push(int stackNum, int val){
-	if (stackPointer[stackNum] + 1 > 0)
-		cout << "out of space" << endl;
-	stackPointer[stackNum]++;
-	array[element(stackNum)] = val;
+	void push(int stackNum, int val){
+		if (stackPointer[stackNum] + 1 > 0)
+			cout << "out of space" << endl;
+		stackPointer[stackNum]++;
+		array[element(stackNum)] = val;
 
-}
+	}
 
-int pop(int stackNum){
-	if (stackPointer[stackNum] == -1)
-		cout << "empty stack" << endl;
-	int value = array[element(stackNum)];
-	stackPointer[stackNum]--;
-	return value;
-}
+	int pop(int stackNum){
+		if (stackPointer[stackNum] == -1)
+			cout << "empty stack" << endl;
+		int value = array[element(stackNum)];
+		stackPointer[stackNum]--;
+		return value;
+	}
+};
 
 
 
+stack Stack;
 int main(){
-	push(1, 4);
-	push(2, 5);
-	cout << pop(2) << endl;
+	Stack.push(1, 4);
+	Stack.push(2, 5);
+	cout << Stack.pop(2) << endl;
 	system("pause");
 	return 0;
 }
