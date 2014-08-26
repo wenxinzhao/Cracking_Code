b301e60780b9f67bf6abb31513862df6b68ee108
terrychenism <hahakuku@hotmail.com>
2014-08-26 00:52:10 -0400

    Create Problem_3.6.cpp

---
+++ Chapter_3_stacks_queues/Problem_3.6.cpp
@@ -0,0 +1,59 @@
+/*
+Write a program to sort a stack in ascending order. 
+You should not make any assumptions about how the stack is implemented. 
+The following are the only functions that should be used to write this program: 
+push | pop | peek | isEmpty.
+
+Terry
+2/15/2014
+*/
+
+#include<iostream>
+#include<stack>
+using namespace std;
+
+stack<int> box1,box2;
+
+stack<int> sort(stack<int>src, stack<int>dst){
+	dst.push(src.top());
+	src.pop();
+	int tmp ;
+	while (!src.empty()){
+		tmp = src.top();
+		src.pop();
+		if (tmp > dst.top()){ //tmp > dst.top
+			dst.push(tmp);
+			
+		}
+		else // temp <= dst.top
+		{
+			
+			while (dst.top() > tmp ){
+					src.push(dst.top());
+					dst.pop();
+					if (dst.empty()) break;					
+			} 
+			
+			dst.push(tmp);			
+			dst.push(src.top());
+			src.pop();
+		}
+	
+	}
+				
+	return dst;
+}
+
+
+void main(){
+	
+	int a[15] = { 43,7,5, 2, 8, 3, 9 ,12,51,21,56,13,34,1,0};
+	for (int i = 0; i < 15; i++)
+	{
+		box1.push(a[i]);
+	}
+	
+	cout << sort(box1, box2).top() << endl;
+	system("pause");
+
+}
