388edd940ad3f44c68c1091de13b6d84e02f1254
terrychenism <hahakuku@hotmail.com>
2014-08-30 16:09:39 -0400

    Update Problem_11.7.cpp

--- Chapter_11_Sorting_Searching/Problem_11.7.cpp
+++ Chapter_11_Sorting_Searching/Problem_11.7.cpp
@@ -26,10 +26,29 @@ int LIS(person A[], int n){
     return len;
 }
 
+int lis(int A[], int n){
+    int *d = new int[n];
+    int len = 1;
+    for(int i=0; i<n; ++i){
+        d[i] = 1;
+        for(int j=0; j<i; ++j)
+            if(A[j]<=A[i] && d[j]+1>d[i])
+                d[i] = d[j] + 1;
+        if(d[i]>len) 
+			len = d[i];
+    }
+    delete[] d;
+    return len;
+}
+
 int main(){
-	person p[] = {{65, 100},{70, 150},{56, 90},{75, 190},{60, 95},{68, 110}};
+	person p[] = {{65, 100},{70, 80},{56, 90},{75, 190},{60, 95},{68, 110}};
 	sort(p , p+6, comp);
 	cout << LIS(p,6) << endl;
+	int A[] = {
+        5, 3, 4, 8, 6, 7
+    };
+    cout<<lis(A, 6)<<endl;
 	system("pause");
 	return 0;
 
